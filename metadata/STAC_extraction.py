aa657bd332e8b845afa108c48cdc5a233ab5282c
Piotr Zaborowski <pzb@bixbite.eu>
2024-10-03 11:36:10 +0200

    stac extensions declared

--- metadata/STAC_extraction.py
+++ metadata/STAC_extraction.py
@@ -8,6 +8,25 @@ import json
 import os
 from pandas.api.types import is_numeric_dtype
 
+wasGeneratedBy = {
+            "provType": "Activity",
+            "id": "surveys:DP-1-S1",
+            "activityType": "InitialSurvey",
+            "endedAtTime": "2023-10-05T05:03:15+01:00",
+            "wasAssociatedWith": "staff:jd234",
+            "used": {
+            "id": "regulations:Act3",
+            "entityType": "Legislation",
+            "wasAttributedTo": "agents:someGovernment",
+            "links": [
+                {
+                "href": "https://some.gov/linktoact/",
+                "rel": "related"
+                }
+            ]
+            }
+            }
+
 def extract_netcdf_metadata(netcdf_file,lon='lon',lat='lat', time='time'):
     # Open the NetCDF file using xarray
     ds = xr.open_dataset(netcdf_file)
@@ -78,24 +97,7 @@ def create_themes_properties(ds, ps):
             "water",
             "ozoother keyword"
         ]
-    ps["wasGeneratedBy"] = {
-            "provType": "Activity",
-            "id": "surveys:DP-1-S1",
-            "activityType": "InitialSurvey",
-            "endedAtTime": "2023-10-05T05:03:15+01:00",
-            "wasAssociatedWith": "staff:jd234",
-            "used": {
-            "id": "regulations:Act3",
-            "entityType": "Legislation",
-            "wasAttributedTo": "agents:someGovernment",
-            "links": [
-                {
-                "href": "https://some.gov/linktoact/",
-                "rel": "related"
-                }
-            ]
-            }
-            }
+    ps["wasGeneratedBy"] = wasGeneratedBy
 
 
 def create_properties(ds):
@@ -111,17 +113,18 @@ def create_cube_variables(ds, ps):
     ps["cube:variables"] = {}
     for v in vars:
         print(v)
-        ps["cube:variables"][v] = {
-            "dimensions": ds.variables[v].dims,
-        }
-        if is_numeric_dtype(ds.variables[v].dtype):
-            ps["cube:variables"][v]["type"] = "data"
-        else:
-            ps["cube:variables"][v]["type"] = "auxiliary"
-        
-        u = ds.variables[v].attrs.get("units")
-        if u:
-            ps["cube:variables"][v]['unit'] = u
+        if any(v == selected_v for selected_v in ['sea_water_salinity','diameter'] ):
+            ps["cube:variables"][v] = {
+                "dimensions": ds.variables[v].dims,
+            }
+            if is_numeric_dtype(ds.variables[v].dtype):
+                ps["cube:variables"][v]["type"] = "data"
+            else:
+                ps["cube:variables"][v]["type"] = "auxiliary"
+            
+            u = ds.variables[v].attrs.get("units")
+            if u:
+                ps["cube:variables"][v]['unit'] = u
 
 
 def create_cube_dimensions(ds, ps):
@@ -172,9 +175,20 @@ def create_stac_item(metadata, netcdf_file, output_folder):
         type="Feature",
         stac_version="1.1.0",
         stac_extensions=["https://stac-extensions.github.io/datacube/v2.2.0/schema.json",
-                         "https://stac-extensions.github.io/themes/v1.0.0/schema.json"],
+                         "https://stac-extensions.github.io/themes/v1.0.0/schema.json",
+                         "https://stac-extensions.github.io/cf/v0.2.0/schema.json",
+                         "https://stac-extensions.github.io/contacts/v0.1.1/schema.json",
+                         "https://stac-extensions.github.io/version/v1.2.0/schema.json",
+                         "https://stac-extensions.github.io/processing/v1.2.0/schema.json"],
         conformsTo=["https://ogcincubator.github.io/geodcat-ogcapi-records/bblock/ogc.geo.geodcat.geodcat-stac-eo/",
-                    "https://ogcincubator.github.io/iliad-apis-features/bblock/ogc.hosted.iliad.api.stac_record"],
+                    "https://ogcincubator.github.io/iliad-apis-features/bblock/ogc.hosted.iliad.api.stac_record",
+                    "https://stac-extensions.github.io/datacube/v2.2.0/schema.json",
+                    "https://stac-extensions.github.io/datacube/v2.2.0/schema.json",
+                    "https://stac-extensions.github.io/themes/v1.0.0/schema.json",
+                    "https://stac-extensions.github.io/cf/v0.2.0/schema.json",
+                    "https://stac-extensions.github.io/contacts/v0.1.1/schema.json",
+                    "https://stac-extensions.github.io/version/v1.2.0/schema.json",
+                    "https://stac-extensions.github.io/processing/v1.2.0/schema.json"],
         properties={**metadata["properties"], **{
             "title": metadata["title"],
             "description": metadata["description"],
@@ -184,13 +198,22 @@ def create_stac_item(metadata, netcdf_file, output_folder):
             "created": datetime.utcnow().isoformat() + "Z",
             "updated": datetime.utcnow().isoformat() + "Z"},
             "rights": "string defining access rights inline or as URL",
-            "applicableLegislation": "string defining applicable legislation inline or as URL",
+            "applicableLegislation": "string defining applicable legislation inline or as URL, use wasGeneratedBy.used for cases whereever you can assign quality measure to particular processing step",
             "convention": "CF-1.6",
             "version": "optional resource behind the record version string",
             "deprecated": False,
             "latest-version": "optional link to the record with latest version",
             "predecessor-version": "optional link to the record with previous version",
-            "successor-version": "optional link to the record with next version"
+            "successor-version": "optional link to the record with next version",
+            "cf:parameter": [
+                {
+                "name": "sea_water_salinity",
+                "schema": "http://vocab.nerc.ac.uk/standard_name/",
+                "unit": "https://qudt.org/2.1/vocab/unit/GM-PER-KiloGM"
+                }
+            ],
+            
+            "dqv:hasQualityMeasurement": "optional description of the quality measures used to generate the content, use wasGeneratedBy.used for cases whereever you can assign quality measure to particular processing step"
         },
         geometry={
             "type": "Polygon",
@@ -241,6 +264,13 @@ def create_stac_item(metadata, netcdf_file, output_folder):
                 href=f"{output_folder}/{_id}.zip"
             ),
             Link(
+                rel="conformance",
+                type="text/html",
+                title="link to the resource confirmance speficication",
+                href=f"https://cfconventions.org/Data/cf-conventions/cf-conventions-1.11/cf-conventions.pdf"
+            ),
+            
+            Link(
                 rel="service",
                 type="application/xml",
                 title="optional link to the web service (not raw file) like OGC Web Map Service (WMS) either one enclosure or service is required in Iliad profile",
@@ -257,6 +287,24 @@ def create_stac_item(metadata, netcdf_file, output_folder):
                 type="text/html",
                 title= "optional link to the license",
                 href=f"https://example.com/license"
+            ),
+            Link(
+                rel="next",
+                type="text/html",
+                title= "optional link to the next in the serie",
+                href=f"https://example.com/next_data_record"
+            ),
+            Link(
+                rel="prev",
+                type="text/html",
+                title= "optional link to the previous in the serie",
+                href=f"https://example.com/prev_data_record"
+            ),
+            Link(
+                rel="parent",
+                type="text/html",
+                title= "optional link to the parent if data in series or other parent in the hierarchy",
+                href=f"https://example.com/prev_data_record"
             )
         ]
     )
